f00b865e0eea11e6302e5e5f661a762e7fea36a1
umangghoghari24 <125755486+umangghoghari24@users.noreply.github.com>
2023-07-01 11:55:37 +0530

    Choose Language

--- lib/LocaleString.dart
+++ lib/LocaleString.dart
@@ -11,21 +11,21 @@ class LocalString extends Translations{
     'en_US': {
       'hello':'Hello Flutter',
       'message': 'Dart Language',
-      'title': 'Flutter Language - Localization',
+      'title': 'Flutter Language ',
       'sub':'Succesful..',
       'changelang' : 'Change Language'
     },
     'hi_IN': {
       'hello':'हेलो फ़्लटर',
       'message': 'डार्ट भाषा',
-      'title': 'स्पंदन भाषा - स्थानीयकरण',
+      'title': 'स्पंदन भाषा ',
       'sub':'सफल..',
       'changelang' : 'भाषा बदलें'
     },
     'guj_gu': {
       'hello':'હેલો ફ્લટર',
       'message': 'ડાર્ટ ભાષા',
-      'title': 'ફ્લટર ભાષા - સ્થાનિકીકરણ',
+      'title': 'ફ્લટર ભાષા ',
       'sub':'સફળ..',
       'changelang' : 'ભાષા બદલો'
     },
